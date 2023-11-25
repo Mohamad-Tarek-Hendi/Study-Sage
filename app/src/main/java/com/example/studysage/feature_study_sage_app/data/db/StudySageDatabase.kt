2f1627975977ecb82d180a10f217f5d9180c8b92
Mohammad Tarek Hendi <t.hindi@outlook.com>
2023-11-25 11:19:57 +0800

    * Add type converter.

--- app/src/main/java/com/example/studysage/feature_study_sage_app/data/db/StudySageDatabase.kt
+++ app/src/main/java/com/example/studysage/feature_study_sage_app/data/db/StudySageDatabase.kt
@@ -2,12 +2,14 @@ package com.example.studysage.feature_study_sage_app.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.studysage.feature_study_sage_app.data.dao.SessionDao
 import com.example.studysage.feature_study_sage_app.data.dao.SubjectDao
 import com.example.studysage.feature_study_sage_app.data.dao.TaskDao
 import com.example.studysage.feature_study_sage_app.data.entity.SessionEntity
 import com.example.studysage.feature_study_sage_app.data.entity.SubjectEntity
 import com.example.studysage.feature_study_sage_app.data.entity.TaskEntity
+import com.example.studysage.feature_study_sage_app.data.type_converter.ColorListConverter
 
 @Database(
     entities = [
@@ -15,8 +17,9 @@ import com.example.studysage.feature_study_sage_app.data.entity.TaskEntity
         TaskEntity::class,
         SessionEntity::class
     ],
-    version = 1
+    version = 2
 )
+@TypeConverters(ColorListConverter::class)
 abstract class StudySageDatabase : RoomDatabase() {
 
     abstract fun subjectDao(): SubjectDao
